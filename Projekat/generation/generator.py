74e61630fa970e7fc142710ed77f3847cc7e31df
MILENA-PC\Milena <mlalic994@gmail.com>
2018-01-17 14:55:13 +0100

    Dodata inicijalna verzija projekta i gitignore fajl #2

---
+++ Projekat/generation/generator.py
@@ -0,0 +1,23 @@
+from jinja2.environment import Environment
+from jinja2.loaders import PackageLoader
+from execute.execute import execute
+from root import root
+import os
+
+def generate(template_name, output_name, render_vars):
+    env = Environment(trim_blocks=True, lstrip_blocks=True, loader=PackageLoader("generation", "templates"))
+    template = env.get_template(template_name)
+    rendered = template.render(render_vars)
+    print(rendered)
+    # i pisemo u fajl
+    file_name = os.path.join(root, "output", output_name)
+    print(file_name)
+    with open(file_name, "w+") as f:
+        f.write(rendered)
+
+def main(debug=False):
+    model = execute(os.path.join(root, "forms"), 'forms.tx', 'example.form', debug, debug)
+    generate("form_template.html", "form.html", {"form": model})
+
+if __name__ == '__main__':
+    main(True)
--- ProjekatJSD/root.py
+++
@@ -1,8 +0,0 @@
-'''
-Created on Dec 20, 2015
-
-@author: xx
-'''
-
-import os
-root = os.path.dirname(__file__)
