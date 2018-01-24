6ac80fdfa96e69ad5d45e1d2393398aa2b825d87
MILENA-PC\Milena <mlalic994@gmail.com>
2018-01-24 14:42:31 +0100

    Dodat prikaz gramatike #5

--- Projekat/generation/generator.py
+++ Projekat/generation/generator.py
@@ -6,6 +6,7 @@ import os
 
 def generate(template_name, output_name, render_vars):
     env = Environment(trim_blocks=True, lstrip_blocks=True, loader=PackageLoader("generation", "templates"))
+    env.filters['readFromFile'] = readFromFile
     template = env.get_template(template_name)
     rendered = template.render(render_vars)
     print(rendered)
@@ -15,9 +16,28 @@ def generate(template_name, output_name, render_vars):
     with open(file_name, "w+") as f:
         f.write(rendered)
 
+def readFromFile():
+    filenameR = os.path.abspath("../forms/forms.tx")
+    fileR = open(filenameR, 'r')
+
+    filenameW = os.path.abspath("../generation/templates/form_gramatika.html")
+    fileW = open(filenameW, 'w')
+
+    fileW.write('{% extends "base.html" %}')
+    fileW.write('{% block body_content %}')
+    fileW.write('<h1>Gramatika rodoslova</h1>')
+    for line in fileR:
+        fileW.write( line + '<br/>')
+    fileW.write('{% endblock %}')
+
+    fileW.close()
+    fileR.close()
+
 def main(debug=False):
     model = execute(os.path.join(root, "forms"), 'forms.tx', 'example.form', debug, debug)
     generate("form_template.html", "form.html", {"rodoslov": model})
+    readFromFile()
+    generate("form_gramatika.html", "gramatika.html", {"rodoslov": model})
 
 if __name__ == '__main__':
     main(True)
